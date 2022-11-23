805024206685da5b49b2a45cd5184de6b4d5eaa6
Darkherobire <111693997+Darkherobire@users.noreply.github.com>
2022-11-24 01:14:37 +0700

    Edit MainMenu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -9,22 +9,6 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private string gameScene1;
     [SerializeField] private string gameScene2;
 
-    private void MainMenuScene()
-    {
-        SceneManager.LoadScene(menuScene);
-    }
-
-    private void GameModeEndless()
-    {
-        SceneManager.LoadScene(gameScene1);
-    }
-
-    private void GameModeTimer()
-    {
-        Debug.Log("Game game...");
-        SceneManager.LoadScene(gameScene2);
-    }
-    
     public void QuitGame() 
     {
         Debug.Log("Quiting game...");
